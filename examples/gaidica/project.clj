577637ad9c43277a8b73be86c15ef3f9662e9d1a
Matthew Carter <m@ahungry.com>
2019-09-15 01:40:04 -0400

    None of the samples work with 1.4.2 seesaw

--- examples/gaidica/project.clj
+++ examples/gaidica/project.clj
@@ -1,5 +1,5 @@
 (defproject gaidica "1.0.0-SNAPSHOT"
   :description "Example Seesaw application"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [seesaw "1.4.2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [seesaw "1.5.0"]
                  [ororo "0.1.0-alpha1"]])
